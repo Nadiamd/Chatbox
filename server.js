325fb122945eeffaadcb1f035127781897a8b7ee
Nadia <nadiamahmoud.09@gmail.com>
2023-02-09 19:46:21 +0100

    delete button commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("express")();
 const port = 3000;
 const http = require("http").Server(app);
 const io = require("socket.io")(http); // package qui permet(au client) de communiquer avec le serveur
+var counter = 0;
 // route express
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -12,9 +13,16 @@ app.get("/style.css", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("connexion");
-  socket.on('message', data => { // on écoute sur le canal "message"
-    console.log(data) // data correspond ici au contenu de notre message 
-    io.emit('message',data); // le serveur renvoie "data" sur le canal "message"
+  socket.on('message', texto => { // on écoute sur le canal "message"
+    console.log(texto) // data correspond ici au contenu de notre message 
+    var newData = {texto,counter}
+    console.log(newData.texto)
+    console.log(newData.counter)
+
+    io.emit('message',newData); // le serveur renvoie "data" sur le canal "message"
+      // création d'un compteur 
+      counter +=1; // on incrémente de 1 à chaque fois
+    
   });
 });
 
