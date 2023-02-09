1bb1cd7c1ee73d23075bd57b621d3ad835e48aff
Nadia <nadiamahmoud.09@gmail.com>
2023-02-09 22:07:18 +0100

    color messages differentiated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const port = 3000;
 const http = require("http").Server(app);
 const io = require("socket.io")(http); // package qui permet(au client) de communiquer avec le serveur
 var counter = 0;
+
 // route express
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -12,17 +13,22 @@ app.get("/style.css", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("connexion");
+  console.log("connected:" + socket.id);
+  socket.emit("yourId", socket.id);
   socket.on("message", (texto) => {
     // on écoute sur le canal "message"
     console.log(texto); // data correspond ici au contenu de notre message
-    var newData = { texto, counter };
+    var sentId = socket.id
+    var newData = { texto, counter,sentId};
     console.log(newData.texto);
     console.log(newData.counter);
 
     io.emit("message", newData); // le serveur renvoie "data" sur le canal "message"
     // création d'un compteur
     counter += 1; // on incrémente de 1 à chaque fois
+  });
+  socket.on("disconnect", function () { 
+    console.log("disconnected" + socket.id);
   });
 
   socket.on("delete", (number) => {
